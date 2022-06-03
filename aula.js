afe1e630f6896f34038f66205d57d0d6f9032a74
Murilo Nepomuceno <murilonep@gmail.com>
2022-06-03 11:54:35 -0300

    inicial

---
+++ aula.js
@@ -0,0 +1,20 @@
+// realiza uma tarefa, não devolve nada
+function dizerNome(){
+    console.log('murilo');
+
+
+};
+
+dizerNome();
+// Faz um cálculo ou operação e retorna algo
+function MultiplicarValor(valor){
+  return valor * 2;
+
+};
+
+// console.log(MultiplicarValor(10));
+
+
+let resultado = MultiplicarValor(5)
+
+console.log(MultiplicarValor)
---
+++ exercicios/aula01.js
@@ -0,0 +1,11 @@
+// let somaSite = 0
+
+// function adicionaSoma (num1, num2 ){
+//     somaSite = num1 + num2
+
+
+// };
+
+// console.log(somaSite);
+// adicionaSoma(10,15);
+// console.log(somaSite);
